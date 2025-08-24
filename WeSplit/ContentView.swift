c50f1f0a856ae3a698b5c7131563b984b525883f
ledoux25 <sanguojoseph17@gmail.com>
2025-08-24 10:27:39 -0400

    WeSplit Project Initialation

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var checkAmount : Double? = nil
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocussed : Bool
+    
+    var valuePerPerson : Double {
+        
+        if let checkAmount = checkAmount {
+            var totalAmount = checkAmount + checkAmount/100 * Double(tipPercentage)
+            print(totalAmount)
+            
+            return totalAmount / Double(numberOfPeople)
         }
-        .padding()
+        return 0.0
+    }
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    
+    var body: some View{
+        NavigationStack {
+            Form{
+                
+                Section("Check Amount")
+                    {
+                        
+                        
+                        TextField(
+                            "$0.00",
+                            value: $checkAmount,
+                            format: .currency(code: "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocussed)
+                    
+                    
+                    
+                    
+                    Picker("Number of people", selection: $numberOfPeople,){
+                        ForEach(2 ..< 100, id: \.self){
+                            Text("\($0) people")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                Section("How much tip do you want to leave ? "){
+                    Picker("Select Tip",  selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                VStack(alignment: .center, spacing: 0.0,){
+                    Text(valuePerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).font(.system(size: 45.0))
+                    Text("/person").foregroundColor(Color.gray)
+                }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center).padding(.vertical, 45)
+            }
+            
+            .toolbar{
+                ToolbarItem(placement: .keyboard){
+                    if amountIsFocussed{
+                        Button("done"){
+                            amountIsFocussed = false
+                        }
+                    }
+                }
+                
+            }
+            .navigationTitle("Wesplit")
+            
+        }
+        
     }
 }
 
